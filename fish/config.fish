3fd413d81fdd340b390dcf3f448c46b503a1944e
kei <kei.adc11.29@gmail.com>
2019-11-09 23:35:30 +0900

    fishとtmux編集

--- fish/config.fish
+++ fish/config.fish
@@ -18,3 +18,20 @@ alias cp 'cp -i'
 alias mv 'mv -i'
 alias mkdir 'mkdir -p'
 
+set -g theme_nerd_fonts yes
+set -g theme_display_git_dirty_verbose yes
+set -g theme_display_docker_machine no
+set -g theme_display_k8s_context yes
+set -g theme_display_hg yes
+set -g theme_display_ruby yes
+set -g theme_display_hostname ssh
+set -g theme_display_vi yes
+set -g theme_display_cmd_duration yes
+set -g theme_title_display_process yes
+set -g theme_date_format "+%a %H:%M"
+set -g theme_avoid_ambiguous_glyphs yes
+set -g theme_powerline_fonts yes
+set -g theme_show_exit_status yes
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt '$ ' 
+set -g theme_display_nvm yes
